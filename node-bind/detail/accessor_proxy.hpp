3497fd6d11d914995c963172f6163bd128e16355
Benaka Moorthi <benaka.moorthi@gmail.com>
2012-01-29 20:35:56 +0000

    Added license & license header.

--- node-bind/detail/accessor_proxy.hpp
+++ node-bind/detail/accessor_proxy.hpp
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2012 Benaka Moorthi
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #if !defined( NODE_BIND_DETAIL_ACCESSOR_PROXY_HPP )
 #define NODE_BIND_DETAIL_ACCESSOR_PROXY_HPP
 
--- node-bind/detail/set_argument_tuple.hpp
+++ node-bind/detail/set_argument_tuple.hpp
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2012 Benaka Moorthi
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #if !defined( NODEBIND_DETAIL_SET_ARGUMENT_TUPLE_HPP )
 #define NODEBIND_DETAIL_SET_ARGUMENT_TUPLE_HPP
 
